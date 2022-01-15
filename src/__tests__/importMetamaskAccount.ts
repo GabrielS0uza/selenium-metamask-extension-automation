ea97b10646e05f63c447e50ef340f6522f2b5289
Gabriel Souza <gabriel-a-souza@hotmail.com>
2022-01-15 12:53:06 -0300

    First commit

---
+++ src/__tests__/importMetamaskAccount.ts
@@ -0,0 +1,26 @@
+import { Options as OptionsChrome } from 'selenium-webdriver/chrome';
+
+import { setupDriver } from '../setup';
+import { BrowsersEnum } from '../utils/_types';
+import * as metamaskWelcomeActions from '../pages/metamask/welcome/actions';
+import { METAMASK_EXTENSION_PATH, FAKE_RECOVERY_PHRASE, FAKE_PASSWORD } from '../utils/_consts';
+
+async function importMetamaskAccount() {
+  const options = new OptionsChrome().addExtensions([METAMASK_EXTENSION_PATH]);
+  let driver = await setupDriver({ browser: BrowsersEnum.Chrome, options, hasExtension: true });
+
+  try {
+    await metamaskWelcomeActions.clickBtnStartNow(driver);
+    await metamaskWelcomeActions.clickBtnImportWallet(driver);
+    await metamaskWelcomeActions.clickBtnNoThanks(driver);
+    await metamaskWelcomeActions.fillRecoveryPhrase(driver, FAKE_RECOVERY_PHRASE);
+    await metamaskWelcomeActions.fillNewPassword(driver, FAKE_PASSWORD);
+    await metamaskWelcomeActions.fillConfirmPassword(driver, FAKE_PASSWORD);
+    await metamaskWelcomeActions.checkTermsOfUse(driver);
+    await metamaskWelcomeActions.clickBtnImport(driver);
+  } finally {
+    await driver.quit();
+  }
+}
+
+importMetamaskAccount();
---
+++ src/utils/_consts.ts
@@ -0,0 +1,8 @@
+import path from 'path';
+
+export const METAMASK_EXTENSION_PATH = path.resolve('src/extensions/10.8.1_0.crx');
+
+export const FAKE_RECOVERY_PHRASE =
+  'letter,coffin,tree,avatar,mask,oven,cry,robot,donut,sky,color,risk';
+
+export const FAKE_PASSWORD = '123456789';
